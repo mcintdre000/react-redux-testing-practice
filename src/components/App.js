b4f1f7d9cee1c4ac29bfd68eb6c2ae79a8dcddb0
mcintdre000 <38018234+mcintdre000@users.noreply.github.com>
2018-10-15 00:08:39 -0700

    Implemented Testing for CommentBox component

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div>
+                <CommentBox />
+                <CommentList />
+            </div>
+        );
+    }
+}
