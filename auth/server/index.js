93a0475a2cda6a6e473c13c0aec331be3c53478b
mcintdre000 <38018234+mcintdre000@users.noreply.github.com>
2018-10-19 01:05:08 -0700

    Finished Auth api server with bcrypt

--- auth/server/index.js
+++ auth/server/index.js
@@ -3,21 +3,22 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
-
-const app = express();
+const cors = require('cors');
 
 // DB Setup
-mongoose.connect('mongodb://localhost:auth/auth', { useNewUrlParser: true });
+mongoose.connect('mongodb://localhost/auth');
 
 // App Setup
 app.use(morgan('combined'));
+app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
 // Server Setup
-const port = process.env.PORT || 9001;
+const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
 console.log('Server listening on:', port);
